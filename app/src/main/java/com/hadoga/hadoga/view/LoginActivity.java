121dc19d03d12b9ce631f5faeb1ca47fc472e7cf
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-10-13 22:48:05 -0600

    feat: agregar register activity

--- app/src/main/java/com/hadoga/hadoga/view/LoginActivity.java
+++ app/src/main/java/com/hadoga/hadoga/view/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.hadoga.hadoga.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Vincula el XML de la activity
         setContentView(R.layout.activity_login);
+
+        // Texto "Regístrate"
+        TextView textGoRegister = findViewById(R.id.textGoRegister);
+
+        // Al hacer clic, abrirá la RegisterActivity
+        textGoRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
