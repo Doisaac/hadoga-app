d7448aa3bf6dd53308d9f455d9303ae96f5c1104
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-10-22 11:33:19 -0600

    feat: agregar lógica para agregar paciente

--- app/src/main/java/com/hadoga/hadoga/model/database/HadogaDatabase.java
+++ app/src/main/java/com/hadoga/hadoga/model/database/HadogaDatabase.java
@@ -7,19 +7,22 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.hadoga.hadoga.model.dao.DoctorDao;
+import com.hadoga.hadoga.model.dao.PacienteDao;
 import com.hadoga.hadoga.model.dao.SucursalDao;
 import com.hadoga.hadoga.model.dao.UsuarioDao;
 import com.hadoga.hadoga.model.entities.Doctor;
+import com.hadoga.hadoga.model.entities.Paciente;
 import com.hadoga.hadoga.model.entities.Sucursal;
 import com.hadoga.hadoga.model.entities.Usuario;
 
-@Database(entities = {Usuario.class, Sucursal.class, Doctor.class}, version = 3)
+@Database(entities = {Usuario.class, Sucursal.class, Doctor.class, Paciente.class}, version = 4)
 public abstract class HadogaDatabase extends RoomDatabase {
     private static volatile HadogaDatabase INSTANCIADB;
 
     public abstract UsuarioDao usuarioDao();
     public abstract SucursalDao sucursalDao();
     public abstract DoctorDao doctorDao();
+    public abstract PacienteDao pacienteDao();
 
     public static HadogaDatabase getInstance(Context context) {
         if (INSTANCIADB == null) {
